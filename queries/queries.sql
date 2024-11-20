5f0bcf4d631faefb97f538a73b38b1e9e8e12574
Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2024-11-20 13:40:51 +0100

    feat: select statement

---
+++ queries/queries.sql
@@ -0,0 +1,12 @@
+-- COLLECTION OF SQL QUERIES
+
+
+-- SELECT STATEMENT
+
+select 1 + 1;
+
+select 'the very pulse of the machine', 10 * 10;
+
+select firstname, lastname, city from employees;
+
+select * from employees;
