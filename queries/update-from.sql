413379933372f06e62bd316cba2e6e7aa0bd704b
Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2024-12-11 09:02:44 +0100

    feat: update from notes

---
+++ queries/update-from.sql
@@ -0,0 +1,13 @@
+select 'update from';
+
+-- update data in one table based on data from another table or a subquery
+
+-- update table_name
+-- set
+-- 	col = val,
+-- 	col = val,
+-- 	col = val
+-- from
+-- 	subquery_or_table
+-- where
+-- 	condition
